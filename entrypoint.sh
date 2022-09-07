ac0d99c0117b9466b6487a5be31b4d8d748ba5f8
Qi Zhang <smallqi1@163.com>
2022-09-07 16:12:54 +0800

    update
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,12 +12,10 @@ if [ -f "/arthas/arthas-agent.jar" ]; then
   echo "File \"/arthas/arthas-agent.jar\" exists"
   exit 0
 else
-  mkdir -p /arthas/conf
-  cp /arthas.properties /arthas/conf/arthas.properties
   echo "Downloading Arthas Java Agent..."
   wget -O /arthas/arthas-bin.zip  https://ghproxy.com/https://github.com/alibaba/arthas/releases/download/arthas-all-"$VERSION"/arthas-bin.zip
-  unzip -o /arthas/arthas-bin.zip
+  unzip -o /arthas/arthas-bin.zip -d /arthas
   rm -f /arthas/arthas.properties
-  cp /arthas/conf/arthas.properties /arthas/arthas.properties
+  cp /arthas.properties /arthas/arthas.properties
   echo "Downloaded Arthas Java Agent"
 fi
